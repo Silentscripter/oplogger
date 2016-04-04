8766bfa2a823fbc8e2f672c8f6e4eb4460673bee
Vito Laera <vito.laera@protechstudio.it>
2016-04-05 00:41:33 +0200

    Fix internal Log model $fillable property

--- src/Models/Log.php
+++ src/Models/Log.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Log extends Model
 {
 
-    protected $fillable = ['user_id', 'operation'];
+    protected $fillable = ['user_id', 'operation', 'ip'];
 }
